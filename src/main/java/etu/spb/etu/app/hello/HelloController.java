596d2e5c4b73b600e442eb0895536c46c6ffdf45
Wisardus <maximus906544@yandex.ru>
2024-09-10 22:34:01 +0300

    fix controller

--- src/main/java/etu/spb/etu/app/hello/HelloController.java
+++ src/main/java/etu/spb/etu/app/hello/HelloController.java
@@ -1,2 +1,15 @@
-package etu.spb.etu.app.hello;public class HelloController {
+package etu.spb.etu.app.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello";
+    }
 }
